730cdc9473958d8e657f7eadccb827a50e5fd3c2
anara01 <anaraerkebaeva2@gmail.com>
2022-05-06 17:36:42 +0300

    added place img

--- places/models.py
+++ places/models.py
@@ -1,28 +1,29 @@
+from distutils.command.upload import upload
 from django.db import models
 from django.contrib.auth.models import User
 
 
 class Place(models.Model):
-	user = models.ForeignKey(
-		to=User, on_delete=models.SET_NULL,
-		null=True, blank=True
-	)
-	name = models.CharField(max_length=255)
-	location = models.CharField(max_length=255)
-	description = models.TextField(null=True, blank=True)
-	created_at = models.DateTimeField(auto_now_add=True)
-	updated_at = models.DateTimeField(auto_now=True)
-	views_count = models.IntegerField(default=0)
-	is_publicated = models.BooleanField(default=True)
+    user = models.ForeignKey(
+        to=User, on_delete=models.SET_NULL,
+        null=True, blank=True
+    )
+    name = models.CharField(max_length=255)
+    location = models.CharField(max_length=255)
+    description = models.TextField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    views_count = models.IntegerField(default=0)
+    is_publicated = models.BooleanField(default=True)
+    img = models.ImageField(upload_to='places', null=True, blank=True)
 
-	def __str__(self):
-		return self.name
+    def __str__(self):
+        return self.name
 
-	class Meta:
-		verbose_name = 'место'
-		verbose_name_plural ='Места'
-		ordering = ['name']
-
+    class Meta:
+        verbose_name = 'место'
+        verbose_name_plural = 'Места'
+        ordering = ['name']
 
 
 class Feedback(models.Model):
@@ -46,7 +47,7 @@ class Feedback(models.Model):
 
     def __str__(self):
         return self.text[:20]
-    
+
     class Meta:
         verbose_name = 'обратная связь'
         verbose_name_plural = 'Обратные связи'
